e8d8a3e7e8fbc4bfa191a3c16749bdd8e0be9af1
Sharan <sharanmeetsekhon@gmail.com>
2019-07-04 21:05:08 +0530

    second

--- vowelsInSubstingOfString.py
+++ vowelsInSubstingOfString.py
@@ -1,23 +1,45 @@
-def findSubs(Str,n):
-    strsall = []
-    counter = 0
-    vowel = ['a','e','i','o','u','A','E','I','O','U']
-    for i in range (n):
-        for j in range(i,n):
-            strs = ''
-            for k in range(i,j+1):
-                strs = strs + Str[k]
-            strsall.append(strs)
-    for i in strsall:
-        for j in i:
-            if (j in vowel):
-                counter = counter +1
-    return counter
+'''
+# Sample code to perform I/O:
 
+name = input()                  # Reading input from STDIN
+print('Hi, %s.' % name)         # Writing output to STDOUT
+
+# Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+'''
+
+# Write your code here
+
+def findSubs(Str,n):
+    i=0
+    j=i
+    strs = ''
+    prvcount = 0
+    finalcount = 0
+    count = 0
+    vowel = ['a','e','i','o','u','A','E','I','O','U']
+    while(i<n):
+        strs = Str[i:j]
+        if(Str[j] in vowel):
+            count+=1
+            prvcount+= count
+        else:
+            prvcount+=count
+        if(j<n-1):
+            j+=1
+        elif(i<=n-1):
+            i+=1
+            j=i
+            finalcount+=prvcount
+            prvcount = 0
+            count = 0
+    return finalcount
 
 tot = input()
 tot = int(tot)
 count = 0
 for i in range(tot):
-    temp = input()
-    print(findSubs(temp,len(temp)))
+   temp = input()
+   print(findSubs(temp,len(temp)))
+
+
+            
